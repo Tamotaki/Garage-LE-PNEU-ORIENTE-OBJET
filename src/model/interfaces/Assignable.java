6e28214952033232bdf7f01e3fcd19c8a235b2a0
Pierre <tamotaki-pierro@outlook.com>
2026-05-12 13:20:23 +0200

    MR02

--- src/model/interfaces/Assignable.java
+++ src/model/interfaces/Assignable.java
@@ -1,3 +1,13 @@
 package model.interfaces;
 
-public interface Assignable {}
+public interface Assignable {
+
+    //Retourne true si l'entité est disponible pour une mission
+    boolean estDisponible();
+
+    //Marque l'entité en mission
+    void affecter();
+
+    //Rend l'entité disponible après mission
+    void liberer();
+}
