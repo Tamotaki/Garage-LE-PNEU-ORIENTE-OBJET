247b22831e60956e307f9ee1471b8a7b9fcb3e48
Tamotaki <tamotaki-pierro@outlook.com>
2026-05-12 13:20:23 +0200

    MR02

---
+++ src/model/EtatVehicule.java
@@ -0,0 +1,6 @@
+package model;
+
+//Etats possibles d'un véhicule
+public enum EtatVehicule { 
+    DISPONIBLE, EN_MISSION, EN_MAINTENANCE, INDISPONIBLE
+}
--- src/model/VehiculeSpecial.java
+++ src/model/VehiculeSpecial.java
@@ -1,5 +1,36 @@
 package model;
 
-import model.interfaces.Urgence;
+public class VehiculeSpecial extends Vehicule {
 
-public class VehiculeSpecial extends Vehicule implements Urgence {}
+    //Champs propre à cette sous-classe
+    private String specialisation;
+
+    //Constructeur qui appelle le constructeur de Vehicule et initialise le champs supplémentaire
+    public VehiculeSpecial(String id, String immatriculation, String marque,
+                           String modele, double kilometrage, String specialisation) {
+        super(id, immatriculation, marque, modele, kilometrage);
+        this.specialisation = specialisation;
+    }
+
+    //Implémentation des méthodes abstraites de Vehicule
+    @Override
+    public String getPermisRequis() {
+        return "CE"; //Permis poids lourd avec remorque
+    }
+
+    @Override
+    public String getType() {
+        return "Spécial";
+    }
+
+    //Méthode getter pour le champ supplémentaire
+    public String getSpecialisation() {
+        return specialisation;
+    }
+
+    // Méthode toString enrichie aec la spécialisation
+    @Override
+    public String toString() {
+        return super.toString() + " | Spécialité: " + specialisation;
+    }
+}
