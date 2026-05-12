8c208a8e44eed02979d4b71b54903a598d6d119e
griffongX <vargahhy@gmail.com>
2026-05-12 20:02:39 +0200

    MR05

--- src/model/MissionLongue.java
+++ src/model/MissionLongue.java
@@ -2,7 +2,7 @@ package model;
 
 import model.interfaces.Trackable;
 
-public class MissionLongue extends Mission implements Facturable, Trackable {
+public class MissionLongue extends Mission implements Trackable {
     private static final double TARIF_PAR_KM = 0.95; // €/km (dégressif sur longue distance)
     private static final double FORFAIT_BASE = 150.0; // forfait fixe longue distance
 
